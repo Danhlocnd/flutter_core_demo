5a204ccb7d4d9899bd7897a8a2ce72fd54b34f3b
Danhlocnd <danhloc.nd@gmail.com>
2020-08-11 17:29:37 -0700

    add call api

---
+++ lib/core/interceptor/interceptor_client.dart
@@ -0,0 +1,91 @@
+import 'dart:io';
+// import 'package:Appnew/src/common/root_api.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
+
+const _defaultConnectTimeout = Duration.millisecondsPerMinute;
+const _defaultReceiveTimeout = Duration.millisecondsPerMinute;
+
+class InterceptorClient {
+  String baseUrl = ""; // chi cho api gốc vào đây / các nhánh con thì thêm ngoai provider
+  Dio _dio;
+  final List<Interceptor> interceptors;
+
+  InterceptorClient(
+    Dio dio, {
+    this.interceptors,
+  }) {
+    _dio = dio ?? Dio();
+    _dio
+      ..options.baseUrl = baseUrl
+      ..options.connectTimeout = _defaultConnectTimeout
+      ..options.receiveTimeout = _defaultReceiveTimeout
+      ..httpClientAdapter;
+    // ..options.headers = {'Content-Type': 'application/json; charset=UTF-8'};
+    if (interceptors?.isNotEmpty ?? false) {
+      _dio.interceptors.addAll(interceptors);
+    }
+    if (kDebugMode) {
+      _dio.interceptors.add(LogInterceptor(
+          responseBody: true,
+          error: true,
+          requestHeader: false,
+          responseHeader: false,
+          request: false,
+          requestBody: false));
+    }
+  }
+
+  Future<dynamic> get(
+    String uri, {
+    Map<String, dynamic> queryParameters,
+    Options options,
+    CancelToken cancelToken,
+    ProgressCallback onReceiveProgress,
+  }) async {
+    try {
+      var response = await _dio.get(
+        uri,
+        queryParameters: queryParameters,
+        options: options,
+        cancelToken: cancelToken,
+        onReceiveProgress: onReceiveProgress,
+      );
+      return response.data;
+    } on DioError catch (e) {
+      if (e.response.statusCode == 404) {
+        print(e.response.statusCode);
+      } else {
+        print(e.message);
+        print(e.request);
+      }
+    }
+  }
+
+  Future<dynamic> post(
+    String uri, {
+    data,
+    Map<String, dynamic> queryParameters,
+    Options options,
+    CancelToken cancelToken,
+    ProgressCallback onSendProgress,
+    ProgressCallback onReceiveProgress,
+  }) async {
+    try {
+      var response = await _dio.post(
+        uri,
+        data: data,
+        queryParameters: queryParameters,
+        options: options,
+        cancelToken: cancelToken,
+        onSendProgress: onSendProgress,
+        onReceiveProgress: onReceiveProgress,
+      );
+      return response.data;
+    } on FormatException catch (_) {
+      throw FormatException("Unable to process the data");
+    } catch (e) {
+      throw e;
+    }
+  }
+}
---
+++ lib/screen/auth/login_provider.dart
@@ -0,0 +1,26 @@
+import 'dart:convert';
+
+// import 'package:Appnew/models/index.dart';
+// import 'package:Appnew/src/interceptor/interceptor_client.dart';
+
+import 'package:dio/dio.dart';
+
+class LoginProvider {
+  // InterceptorClient _dioClient;
+
+  // LoginProvider() {
+  //   var dio = Dio();
+  //   _dioClient = InterceptorClient(dio);
+  // }
+
+  // Future<Allbums> getAllBums() async {
+  //   try {
+  //     final response = await _dioClient.get("albums");
+
+  //     return Allbums.fromJson(response);
+  //   } catch (error, stacktrace) {
+  //     print("Exception occured: $error stackTrace: $stacktrace");
+  //     // return User.withError("$error");
+  //   }
+  // }
+}
